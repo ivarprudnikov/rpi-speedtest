9b88382ff3f8c3eb4bc01f1cc3f869347a05d714
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-10-12 13:35:03 +0100

    post data to API

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-require('../')({ raw: true });
+const util = require('util');
+require('../')({ raw: true, send: true })
+  .then((success) => {
+    console.log('ok', util.inspect(success));
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error('error', util.inspect(error));
+    process.exit(1);
+  });
