39052e443737ccda9605a424f45f6511034aca70
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-10-13 11:41:11 +0100

    eslint

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const http = require('http')
-const { PORT = 3000, UP_STAGE } = process.env
+const http = require('http');
+const { PORT = 3000, UP_STAGE } = process.env;
 
 http.createServer((req, res) => {
-  res.end('Hello World from ' + UP_STAGE)
-}).listen(PORT)
-
+  res.end('Hello World from ' + UP_STAGE);
+}).listen(PORT);
