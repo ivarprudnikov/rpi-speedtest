f2854fda9822a75d17baf204475ff0c4b4079661
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-10-23 23:29:21 +0100

    prepare to store details

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const UNPROCESSABLE_ENTITY = 422;
 const NOT_FOUND = 404;
 const CREATED = 201;
 const ERROR = 400;
+const S3 = require('aws-sdk/clients/s3');
+const S3_BUCKET = 'payloads.lila.io';
+const s3Instance = new S3({
+  apiVersion: '2006-03-01',
+  region: 'eu-west-1'
+});
 
 function isEndpoint (req) {
   const { url, method } = req;
@@ -13,12 +19,20 @@ function isEndpoint (req) {
   return reqUrlPath === config.api.speedPath && method === config.api.method.toUpperCase();
 }
 
+function escape (val) {
+  return (val + '').replace(/[^a-z0-9_-]+/ig, '-');
+}
+
 const server = module.exports = http.createServer((req, res) => {
   if (isEndpoint(req)) {
     getRawBody(req, { limit: config.api.maxBytes })
       .then(function (buf) {
         try {
-          JSON.parse(buf.toString());
+          const payload = JSON.parse(buf.toString());
+          const group = escape(payload.group);
+          const device = escape(payload.device);
+          const jsonText = JSON.stringify(payload.data);
+
           res.statusCode = CREATED;
         } catch (e) {
           res.statusCode = UNPROCESSABLE_ENTITY;
