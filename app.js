d3a0a9c37fe024c3d7a2a1754ef0a2f5fc53cbda
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-11-24 15:52:31 +0000

    Use express.js instead of manual request processing

--- app.js
+++ app.js
@@ -1,45 +1,52 @@
-const http = require('http');
-const getRawBody = require('raw-body');
 const { PORT = 3000 } = process.env;
-const config = require('./config');
 const db = require('./db');
+const config = require('./config');
+const express = require('express');
+const app = express();
+const http = require('http');
+const util = require('util');
+const getRawBody = require('raw-body');
+const SERVER_ERROR = 500;
 const UNPROCESSABLE_ENTITY = 422;
 const NOT_FOUND = 404;
 const CREATED = 201;
 const ERROR = 400;
 
-function isEndpoint (req) {
-  const { url, method } = req;
-  const reqUrlPath = url.split('?')[0].split('#')[0];
-  return reqUrlPath === config.api.speedPath && method === config.api.method.toUpperCase();
-}
+const server = module.exports = http.createServer(app);
 
-const server = module.exports = http.createServer((req, res) => {
-  if (isEndpoint(req)) {
-    getRawBody(req, { limit: config.api.maxBytes })
-      .then(function (buf) {
-        let payload;
-        try {
-          payload = JSON.parse(buf.toString());
-        } catch (e) {
-          res.statusCode = UNPROCESSABLE_ENTITY;
+app[config.api.method](config.api.speedPath, (req, res) => {
+  getRawBody(req, { limit: config.api.maxBytes })
+    .then(function (buf) {
+      let payload;
+      try {
+        payload = JSON.parse(buf.toString());
+      } catch (e) {
+        res.statusCode = UNPROCESSABLE_ENTITY;
+        res.end();
+        return;
+      }
+      return db.insertRow(payload.device, payload.data)
+        .then(() => {
+          res.statusCode = CREATED;
           res.end();
-          return;
-        }
-        return db.insertRow(payload.device, payload.data)
-          .then(() => {
-            res.statusCode = CREATED;
-            res.end();
-          });
-      })
-      .catch(function (err) {
-        res.statusCode = err.statusCode || ERROR;
-        res.end(err && err.message);
-      });
-  } else {
-    res.statusCode = NOT_FOUND;
-    res.end();
+        });
+    })
+    .catch(function (err) {
+      res.statusCode = err.statusCode || ERROR;
+      res.end(err && err.message);
+    });
+});
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
   }
+  const code = err.status || SERVER_ERROR;
+  if (code >= 500) {
+    console.log((new Date()).toISOString(), '[ERROR]', util.inspect(err));
+  }
+  res.statusCode = NOT_FOUND;
+  res.end();
 });
 
 if (!module.parent) {
